37101ee673c12a4f8cf0c5baa66f1d5db1c86ced
ymshreyas <ymshreyas@gmail.com>
2026-01-03 23:22:19 +0530

    Task-6 Completed

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { Provider } from 'react-redux'
-import store from './store.js'
+
+// The Provider component from the react-redux library is already imported. This component enables all components in the application to access the Redux store.
+import { Provider } from 'react-redux';
+
+// The Redux store is imported from the store.js file. This store holds the application's state, using the reducer defined in the CartSlice.jsx file.
+import store from './store.js';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    {/* The App component is wrapped with the Provider component, with the Redux store passed as a prop. This allows all components in the app to access and interact with the global state managed by Redux. */}
     <Provider store={store}>
-    <App />
+      <App />
     </Provider>
   </React.StrictMode>,
 )
